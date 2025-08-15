19e66f9698b90f775d4545e1f35fad5a0c5b711c
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-08-14 21:00:05 -0300

    Tratamento de exceções

---
+++ src/main/java/com/wagnerquadros/usuario/infrastructure/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.wagnerquadros.usuario.infrastructure.exceptions;
+
+import com.wagnerquadros.usuario.infrastructure.exceptions.ConflictException;
+import com.wagnerquadros.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.wagnerquadros.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
